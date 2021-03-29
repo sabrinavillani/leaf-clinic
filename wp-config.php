e69f4047be0dd89bb26789b5a29eb2cadcdb596d
Sabrina Villani <srmp.villani@gmail.com>
2021-03-29 19:13:35 +0100

    revert wp-config and static website plugin

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', $_ENV["DB_NAME"] );
+define( 'DB_NAME', 'leaf-clinic' );
 
 /** MySQL database username */
-define( 'DB_USER', $_ENV["DB_USER"] );
+define( 'DB_USER', 'root' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', $_ENV["DB_PASSWORD"] );
+define( 'DB_PASSWORD', 'root' );
 
 /** MySQL hostname */
-define( 'DB_HOST', $_ENV["DB_HOST"] );
+define( 'DB_HOST', 'localhost' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
